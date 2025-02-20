9b885d880039d18e8093f7d0c821eee859d16746
Edward-Zn <edward.zoravarian@gmail.com>
2025-02-20 15:44:30 +0100

    Main app view. Basic CSS

--- resources/views/pageA.blade.php
+++ resources/views/pageA.blade.php
@@ -1,3 +1,7 @@
+@extends('app')
+
+@section('content')
+<div class="container">
 <h2>Welcome, {{ $user->username }}</h2>
 <p>Your unique link: <a href="{{ $user->unique_link }}">{{ $user->unique_link }}</a></p>
 <p>Expires on: {{ $user->link_expires_at }}</p>
@@ -14,4 +18,6 @@
 
 <button onclick="window.location.href='/lucky'">I'm Feeling Lucky!</button>
 
-<p><button onclick="window.location.href='/'">New User</button><p>
+<button onclick="window.location.href='/'">New User</button>
+</div>
+@endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,6 +1,13 @@
-<form method="POST" action="/register">
-    @csrf
-    <input type="text" name="username" placeholder="Username" required>
-    <input type="text" name="phone" placeholder="Phone Number" required>
-    <button type="submit" class="btn btn-blue">Register</button>
-</form>
+@extends('app')
+
+@section('content')
+<div class="container">
+    <h2>New User</h2>
+    <form method="POST" action="/register">
+        @csrf
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="text" name="phone" placeholder="Phone Number" required>
+        <button type="submit" class="btn btn-blue">Register</button>
+    </form>
+</div>
+@endsection
